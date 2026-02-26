d22632ec0193bd0573dd188fb58fe86da4d0559e
fabriciosilva1001 <josefabriciopereira02@gmail.com>
2026-02-26 14:35:53 -0300

    Add files via upload

---
+++ UC- Algoritimo (26.02.26)/calculo.py
@@ -0,0 +1,6 @@
+base = float(input("Digite o primeiro valor: "))
+altura = float(input("Digite o primeiro valor: "))
+
+area = base * altura
+
+print(area)
---
+++ UC- Algoritimo (26.02.26)/exemplo.py
@@ -0,0 +1,7 @@
+aluno = input("Digite seu nome: ")
+periodo = int(input("Digite em que periodo você está: "))
+ira = float(input("Digite seu IRA: "))
+
+print(type(aluno))
+print(type(periodo))
+print(type(ira))
