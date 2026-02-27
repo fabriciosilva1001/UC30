e4ea39ecbf1dc88c6ef827baa082945159dd0ca1
fabriciosilva1001 <josefabriciopereira02@gmail.com>
2026-02-27 16:10:12 -0300

    Add files via upload

---
+++ UC- Algoritimo (27.02.26)/condicionais.py
@@ -0,0 +1,41 @@
+nome = input("Digite o seu nome completo: ")
+if len(nome) > 50:
+    print("Seu nome é grande, ele possui mais de 50 caracteres. ")
+    print(f"Ele possui {len(nome)}caracteres")
+
+
+    valor= int(input("Digite um número: "))
+if valor % 2 == 0:
+     print("O valor é par")
+
+else:
+    print("O valor é impar")
+ 
+#---------------------------------------------
+
+nota = float(input("Digite sua nota"))
+
+if nota <= 10
+    print("Passou")
+else:
+    if nota == 8:
+        print("Atenção")
+    else:
+        print("Reprovado")
+
+nota = float(input("Digite sua not":))
+
+if nota <= 10:
+    print("Passou!")
+elif nota == 6:
+    print("Recuperação")
+else:
+    print("Reprovado")
+
+
+ idade = int(input("Digite a idade: "))
+
+ if idade >= 16 and idade <= 69:
+    print("Faixa permitida")
+else:
+    print("Fora da faixa")
