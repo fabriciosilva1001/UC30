51e0edf6e017c174e3f8be91928c52abf9870a32
fabriciosilva1001 <josefabriciopereira02@gmail.com>
2026-03-04 11:13:50 -0300

    Add files via upload

---
+++ UC-Algoritimo (03.03.2026)/UC-Algoritimo MANHÃ (04.03.26)/atividade.py
@@ -0,0 +1,9 @@
+compras = ["arroz", "feijão", "leite", "pão", "ovos"]
+
+print(compras)
+
+novo_item = input("Digite um novo item para adicionar à lista: ")
+
+compras.append(novo_item)
+
+print(compras)
